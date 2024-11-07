f4474ed9814e30c48945e28a881dc0fb1a57f2c8
DESKTOP-FEMV3QK\antua <antuandesent2010@gmail.com>
2024-11-07 18:00:09 +0200

    init

--- Django_projects/dominos_pizza/members/models.py
+++ Django_projects/dominos_pizza/members/models.py
@@ -1,12 +1,48 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    email = models.EmailField(max_length=255)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=255,unique=True)
     password = models.CharField(max_length=255)
-    full_name = models.CharField(max_length=255)
-    bio = models.CharField(max_length=2024)
-    avatar = models.ImageField(upload_to='avatars/')
+    full_name = models.CharField(max_length=255,blank=True)
+    bio = models.CharField(max_length=2024,blank=True)
+    avatar = models.ImageField(upload_to='avatars/',blank=True)
 
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+    def __str__(self):
+        return self.email
     class Meta:
-        db_table = "User"
-
+        db_table = "users"
